ad660d40882ca7097fba338585b02cf11ef17dd8
Osman <avarets1234@gmail.com>
2026-04-03 11:29:13 +0300

    цветные логи через ANSI-коды в консольном энкодере

--- internal/shared/logger/logger.go
+++ internal/shared/logger/logger.go
@@ -1,11 +1,36 @@
 package logger
 
 import (
+	"os"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-// New creates a zap.Logger with the given level (debug/info/warn/error).
+const (
+	colorReset  = "\033[0m"
+	colorRed    = "\033[31m"
+	colorYellow = "\033[33m"
+	colorCyan   = "\033[36m"
+	colorGray   = "\033[37m"
+)
+
+func coloredLevel(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+	var color string
+	switch level {
+	case zapcore.DebugLevel:
+		color = colorGray
+	case zapcore.InfoLevel:
+		color = colorCyan
+	case zapcore.WarnLevel:
+		color = colorYellow
+	default: // error, fatal, panic
+		color = colorRed
+	}
+	enc.AppendString(color + level.CapitalString() + colorReset)
+}
+
+// New creates a zap.Logger with colored console output and the given level.
 func New(level string) *zap.Logger {
 	var lvl zapcore.Level
 	switch level {
@@ -19,9 +44,21 @@ func New(level string) *zap.Logger {
 		lvl = zapcore.InfoLevel
 	}
 
-	cfg := zap.NewProductionConfig()
-	cfg.Level.SetLevel(lvl)
+	encCfg := zapcore.EncoderConfig{
+		TimeKey:        "T",
+		LevelKey:       "L",
+		MessageKey:     "M",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    coloredLevel,
+		EncodeTime:     zapcore.TimeEncoderOfLayout("15:04:05.000"),
+		EncodeDuration: zapcore.StringDurationEncoder,
+	}
 
-	log, _ := cfg.Build()
-	return log
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encCfg),
+		zapcore.AddSync(os.Stdout),
+		lvl,
+	)
+
+	return zap.New(core)
 }
